a04fdb876ca8d0b8f04f3eb9a96078ffb2caae3a
syun <new.type.106.work@gmail.com>
2022-06-02 16:11:37 +0900

    first comiit

---
+++ app/Http/Controllers/FolderController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Http\Requests\CreateFolder;
+
+use APP\Models\Task;
+use App\Models\Folder;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class FolderController extends Controller
+{
+    public function showCreateForm()
+    {
+        return view('folders/create');
+    }
+    public function create(CreateFolder $request)
+{
+    // フォルダモデルのインスタンスを作成する
+    $folder = new Folder();
+    // タイトルに入力値を代入する
+    $folder->title = $request->title;
+    // インスタンスの状態をデータベースに書き込む
+    Auth::user()->folders()->save($folder);
+    $folder->save();
+
+    return redirect()->route('tasks.index', [
+        'id' => $folder->id,
+    ]);
+}
+}
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\FolderController;
+use App\Http\Controllers\HomeController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
+    Route::post('/folders/create', [FolderController::class, 'create']);
+    Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+    Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
+    Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
+    Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
+    Route::get('/', [HomeController::class, 'index'])->name('home');
+});
+
+
+Auth::routes();
